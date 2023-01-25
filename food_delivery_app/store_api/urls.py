ba029137bb5b1f7715e329c86fa8635c059b3e31
Kmerge Dosdos <kpdosdos@gmail.com>
2023-01-25 17:53:52 +0800

    remove unused imports

--- food_delivery_app/store_api/urls.py
+++ food_delivery_app/store_api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import StoreViewSet
-from menu_api.views import MenuConfigDetail, MenuList, CategoryList
 
 router = DefaultRouter()
 router.register(r'stores', StoreViewSet, basename='store')

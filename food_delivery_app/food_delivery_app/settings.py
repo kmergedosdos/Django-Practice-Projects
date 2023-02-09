674c8e563264efb1ae3e05c662c9d03fcecfaf5e
Kmerge Dosdos <kpdosdos@gmail.com>
2023-02-09 15:02:35 +0800

    add comments

--- food_delivery_app/food_delivery_app/settings.py
+++ food_delivery_app/food_delivery_app/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third-party apps
     'rest_framework',
+    # created apps
     'store_api',
     'menu_api'
 ]

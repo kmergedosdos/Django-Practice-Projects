23a56adecdf19319fdad222411ac63653cfdacca
Kmerge Dosdos <kpdosdos@gmail.com>
2023-02-13 18:20:36 +0800

    remove unnecessary code

--- food_delivery_app/menu_api/views.py
+++ food_delivery_app/menu_api/views.py
@@ -6,26 +6,6 @@ from .models import MenuConfig, Menu, Category, Item
 from .serializers import MenuConfigSerializer, MenuSerializer, CategorySerializer, ItemSerializer
 
 # Create your views here.
-class MenuConfigViewSet(viewsets.ModelViewSet):
-  """
-  A ViewSet for viewing and editing Menus.
-  """
-  serializer_class = MenuConfigSerializer
-
-  def get_queryset(self):
-    return MenuConfig.objects.filter(store=self.kwargs['store_pk'])
-  
-  def list(self, request, *args, **kwargs):
-    print(self.kwargs)
-    instance = self.get_queryset().get(store=self.kwargs['store_pk'])
-    serializer = MenuConfigSerializer(instance)
-    print(serializer.data)
-    return Response(serializer.data)
-  
-  def update(self, request, *args, **kwargs):
-    print(self.kwargs)
-    return Response('updated menu config')
-  
 
 class MenuConfigDetail(generics.RetrieveUpdateAPIView):
   """

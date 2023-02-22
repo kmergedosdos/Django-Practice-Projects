cafee9759e05bbd7109c143d430a8bfae17cc31d
Kmerge Dosdos <kpdosdos@gmail.com>
2023-02-22 15:28:34 +0800

    added validation to UserCreation serializer

--- food_delivery_app/food_delivery_app/settings.py
+++ food_delivery_app/food_delivery_app/settings.py
@@ -49,6 +49,10 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'authentication.User'
 
+REST_FRAMEWORK = {
+    'NON_FIELD_ERRORS_KEY': 'errors'
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',

fdd65cb51e15c2075548f75b6e5a6ea86abe3aa0
Kmerge Dosdos <kpdosdos@gmail.com>
2023-02-27 15:26:07 +0800

    custom user manager

--- food_delivery_app/authentication/models.py
+++ food_delivery_app/authentication/models.py
@@ -1,7 +1,48 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+  AbstractUser, BaseUserManager
+)
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+  
+  def create_user(self, username, email, password, **extra_fields):
+    if not username:
+      raise ValueError('The username must be set')
+    if not email:
+      raise ValueError('The email must be set')
+    
+    user = self.model(
+      username=username,
+      email=self.normalize_email(email),
+      **extra_fields
+    )
+
+    user.set_password(password)
+    user.save()
+
+    return user
+
+  def create_superuser(self, username, email, password, **extra_fields):
+    extra_fields.setdefault('is_staff', True)
+    extra_fields.setdefault('is_superuser', True)
+    extra_fields.setdefault('is_active', True)
+
+    if extra_fields.get('is_staff') is not True:
+      raise ValueError('Superuser must have is_stuff=True')
+    if extra_fields.get('is_superuser') is not True:
+      raise ValueError('Superuser must have is_superuser=True')
+    if extra_fields.get('is_active') is not True:
+      raise ValueError('Superuser must have is_active=True')
+    
+    return self.create_user(username, email, password, **extra_fields)
+
 
 class User(AbstractUser):
-  pass
+  username = models.CharField(max_length=25, unique=True)
+  email = models.EmailField(max_length=255, unique=True)
+
+  objects = CustomUserManager()
+  
+  USERNAME_FIELD = 'username'
+  REQUIRED_FIELDS = ['email']
--- food_delivery_app/authentication/serializers.py
+++ food_delivery_app/authentication/serializers.py
@@ -11,10 +11,3 @@ class UserCreationSerializer(serializers.ModelSerializer):
       'email',
       'password'
     ]
-
-  def validate(self, attrs):
-    email_exists = User.objects.filter(email=attrs['email']).exists()
-    if email_exists:
-      raise serializers.ValidationError('email is already in used.')
-    
-    return super().validate(attrs)
